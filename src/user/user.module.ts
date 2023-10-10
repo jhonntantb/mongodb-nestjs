0f75eaa884c91f5867767dd0c6d470b35a5e05df
jhonntantb <jhonntantb@gmail.com>
2023-10-10 18:15:59 -0300

    fix user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,12 +3,13 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { UserRepository } from './user.repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserRepository],
 })
 export class UserModule {}
